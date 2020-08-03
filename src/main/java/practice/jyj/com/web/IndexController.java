5a2669628cd4afa0ad70ea486b768b20e7072b4e
yoonjaej3 <yoonjaej3@gmail.com>
2020-08-04 05:56:38 +0900

    ch 4

--- src/main/java/practice/jyj/com/web/IndexController.java
+++ src/main/java/practice/jyj/com/web/IndexController.java
@@ -4,7 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import practice.jyj.com.service.posts.PostsService;
+import practice.jyj.com.web.dto.PostsResponseDto;
 
 @RequiredArgsConstructor
 @Controller
@@ -20,5 +22,15 @@ public class IndexController {
     @GetMapping("/posts/save")
     public String postsSave(){
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model){
+
+        PostsResponseDto dto=postsService.findById(id);
+        model.addAttribute("post",dto);
+
+        return "posts-update";
+
     }
 }
