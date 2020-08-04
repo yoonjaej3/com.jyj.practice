4993ceddabb134dd96606a1fd80f6d2ed9cea492
yoonjaej3 <yoonjaej3@gmail.com>
2020-08-04 18:55:57 +0900

    ch 5

--- src/main/java/practice/jyj/com/config/auth/dto/OAuthAttributes.java
+++ src/main/java/practice/jyj/com/config/auth/dto/OAuthAttributes.java
@@ -31,7 +31,12 @@ public class OAuthAttributes {
     public static OAuthAttributes of(String registrationId,
                                      String userNameAttributeName,
                                      Map<String,Object> attributes){
-        return ofGoogle(userNameAttributeName,attributes);
+
+        if("naver".equals(registrationId)) {
+            return ofNaver("id", attributes);
+        }
+
+        return ofGoogle(userNameAttributeName, attributes);
     }
     public static OAuthAttributes ofGoogle(String
                                            userNameAttributeName, Map<String,Object> attributes){
@@ -44,6 +49,17 @@ public class OAuthAttributes {
                 .build();
     }
 
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
     public User toEntity(){
         return User.builder()
                 .name(name)
