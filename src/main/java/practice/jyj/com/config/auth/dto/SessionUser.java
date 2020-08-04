7544979ac77eeaa03c158d1ba34d79de7bde8521
yoonjaej3 <yoonjaej3@gmail.com>
2020-08-04 17:17:41 +0900

    fixed untracked files

---
+++ src/main/java/practice/jyj/com/config/auth/dto/SessionUser.java
@@ -0,0 +1,20 @@
+package practice.jyj.com.config.auth.dto;
+
+import lombok.Getter;
+import practice.jyj.com.domain.user.User;
+
+import java.io.Serializable;
+
+@Getter
+public class SessionUser implements Serializable {
+
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(User user) {
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getPicture();
+    }
+}
